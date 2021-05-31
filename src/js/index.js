68ccb501b9f97c9b47bc7de363460d3bca09207b
yuki_sagara <sagara@impv.co.jp>
2021-05-31 15:45:35 +0900

    headerを追加

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,11 @@
 
 const $ = require("jquery");
 
-$(function() {
-    console.log('ok');
+window.addEventListener('scroll', function(){
+    const header_element = document.getElementsByClassName('header');
+    if( 72 < window.scrollY ){
+        header_element[0].classList.add("bg");
+    }else{
+        header_element[0].classList.remove("bg");
+    }
 });
