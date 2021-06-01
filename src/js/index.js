0d31f26625a5d6b2b71e6a18d026972b995c1484
yuki_sagara <sagara@impv.co.jp>
2021-06-01 21:18:09 +0900

    navigationを追加

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,29 @@ window.addEventListener('scroll', function(){
         header_element[0].classList.remove("bg");
     }
 });
+window.onload = function() {
+    const navBtn = document.getElementById('navTrigger');
+    const navigation = document.getElementById('navigation');
+    const navItem = document.querySelectorAll('.nav-btn');
+
+    navBtn.addEventListener("click", function() {
+        if(navBtn.classList.contains('active')){
+            navClose()
+        }else{
+            navOpen();
+        }
+    });
+    navItem.forEach(function(navItem) {
+        navItem.addEventListener('click', function() {
+            navClose();
+        });
+    });
+    function navOpen() {
+        navBtn.classList.add('active');
+        navigation.classList.add('active');
+    }
+    function navClose() {
+        navBtn.classList.remove('active');
+        navigation.classList.remove('active');
+    }
+ }
